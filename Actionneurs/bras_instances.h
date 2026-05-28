caa85f1d92ca41a62e17a54dc93ea73a45255e52
achille <achille.theux@gmail.com>
2026-05-28 16:26:49 +0200

    plus nettoyage

--- Actionneurs/bras_instances.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BRAS_INSTANCES_H
-#define __BRAS_INSTANCES_H
-
-#include "bras.h"
-
-// -------------------------------------------------------------------
-// Wrappers pour l'interpréteur (lisent les paramètres u32 puis délègu
-extern Bras_t* bras_instances[3];
-#endif /* __BRAS_INSTANCES_H */
